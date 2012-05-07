695890352f895215c0aea343190ba2f863f871dc
EdGruberman <ed@rjump.com>
2012-05-06 23:16:29 -0700

    FIX: /reply command would not work after reconnect

--- src/edgruberman/bukkit/messageformatter/commands/Reply.java
+++ src/edgruberman/bukkit/messageformatter/commands/Reply.java
@@ -5,6 +5,10 @@ import java.util.Map;
 
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import edgruberman.bukkit.messageformatter.Main;
@@ -13,7 +17,7 @@ import edgruberman.bukkit.messageformatter.commands.util.Context;
 import edgruberman.bukkit.messageformatter.commands.util.Parser;
 import edgruberman.bukkit.messagemanager.MessageLevel;
 
-public final class Reply extends Action {
+public final class Reply extends Action implements Listener {
 
     /**
      * Index of last private messages sent From, To.
@@ -22,22 +26,33 @@ public final class Reply extends Action {
 
     public Reply(final JavaPlugin plugin) {
         super(plugin, "reply");
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
     public boolean perform(final Context context) {
         if (context.arguments.size() < 1) return false;
 
-        final CommandSender recipient = Reply.last.get(context.sender);
+        CommandSender recipient = Reply.last.get(context.sender);
         if (recipient == null || (recipient instanceof Player && !((Player) recipient).isOnline())) {
             Main.messageManager.send(context.sender, "Unable to send reply; Last sender not found", MessageLevel.WARNING, false);
             return true;
+        }
+
+        if (recipient instanceof Player) {
+            // Ensure latest instance of CommandSender is used for players after they reconnect
+            recipient = this.handler.command.getPlugin().getServer().getPlayerExact(recipient.getName());
         }
 
         String message = Parser.join(context.arguments.subList(0, context.arguments.size())).trim();
         message = Main.formatColors(context.sender, message);
         Tell.send(recipient, context.sender, message);
         return true;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerQuit(final PlayerQuitEvent quit) {
+        Reply.last.remove(quit.getPlayer());
     }
 
 }
